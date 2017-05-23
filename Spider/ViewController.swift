5a2873f84b93f7f09aeca32f2e54e47685c40d10
bannzai <hiroseyuudai@hiroseyudais-macbookpro.local>
2017-05-23 12:11:58 +0900

    Initial Commit

---
+++ Spider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spider
+//
+//  Created by 廣瀬雄大 on 2017/05/23.
+//  Copyright © 2017年 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
